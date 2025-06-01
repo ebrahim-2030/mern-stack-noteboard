e57a98446be6b9f5ef079736a6ec327f3b629e98
ebrahim <ebrahimasil@gmail.com>
2025-06-01 15:22:48 +0430

    prepared the for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
+import path from "path";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import ratelimiter from "./middleware/rateLimiter.js";
@@ -11,12 +12,16 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+const __dirname = path.resolve();
+
 // enable CORS for frontend at localhost:5173
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 // parse json requests body
 app.use(express.json());
@@ -24,10 +29,15 @@ app.use(express.json());
 // apply rate limiter middleware to control request rate
 app.use(ratelimiter);
 
-
 // notes middleware to handle api/notes endpoints
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 // connecte to MongoDB & run the server on port 5001
 connectDB().then(() => {
   app.listen(PORT, () => {
